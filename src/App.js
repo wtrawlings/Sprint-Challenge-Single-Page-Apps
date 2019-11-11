7b369c46ec3452196a8b4b01128f40f15e375042
wtrawlings <rawlings.warren@gmail.com>
2019-11-10 21:20:48 -0500

    placed router and links in header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage.js";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <WelcomePage />
       <CharacterList />
     </main>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { BrowserRouter as Route, Link } from "react-router-dom";
+import CharacterList from "./CharacterList";
+import WelcomePage from "./WelcomePage";
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <Link exact to="/"><h2>home</h2></Link>
+      <Link exact to="/characters"><h2>characters</h2></Link>
+      <Route exact path="/" component={WelcomePage} />
+    <Route exact path="/characters" component={CharacterList} />
     </header>
+    
+    
+    
   );
 }
