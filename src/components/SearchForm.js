4dccb35cf8a45aa91dd011c100977c2aa0f7a95f
wtrawlings <rawlings.warren@gmail.com>
2019-11-10 19:05:32 -0500

    here we go!

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,7 +4,16 @@ export default function SearchForm() {
  
   return (
     <section className="search-form">
-     // Add a search form here
+    <label htmlFor="search">Search by Name</label>
+     <input 
+      type="text"
+      name="search"
+      id="search"
+      value={this.state.search}
+      onChange={this.updateSearch.bind(this)} 
+      //check your onChange and value
+      />
+    
     </section>
   );
 }
